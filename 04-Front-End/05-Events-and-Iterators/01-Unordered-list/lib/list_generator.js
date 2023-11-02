aba9000e85352bb0b327b4101ebb259e7ced1ffd
Matt <mprevell97@gmail.com>
2023-11-02 12:21:02 +0000

    first challenge done

--- 04-Front-End/05-Events-and-Iterators/01-Unordered-list/lib/list_generator.js
+++ 04-Front-End/05-Events-and-Iterators/01-Unordered-list/lib/list_generator.js
@@ -6,10 +6,16 @@ import runChallenges from "../spec/list_generator_examiner.js";
 
 const listItem = (content) => {
   // TODO: return the proper <li> HTML tag with its content (as a string)
+  return `<li class="list-group-item">${content}</li>`;
 };
+
+// Below is the one-liner version:
+// const listItem = content => `<li class="list-group-item">${content}</li>`;
 
 const unorderedList = (items) => {
   // TODO: return the proper <ul> markup (as a string)
+  const content = items.map((item) => listItem(item)).join("\n  ");
+  return `<ul class="list-group">${content}</ul>`;
 };
 
 // Do not remove these lines:
@@ -17,8 +23,11 @@ if (typeof window === "object") {
   document.addEventListener("DOMContentLoaded", () => {
     // TODO: call unorderedList() passing the right element
     // TODO: display the list in the browser
+    const groceries = ["milk", "butter", "bread"];
+    const list = document.getElementById("list");
+    list.innerHTML = unorderedList(groceries);
   });
-};
+}
 
 runChallenges(listItem, unorderedList); // Do not remove.
 export { listItem, unorderedList };
