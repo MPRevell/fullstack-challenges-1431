e36eb08b85158ff8044b9b0226711c4b2f2389c9
Matt <mprevell97@gmail.com>
2023-11-27 20:19:49 +0000

    thid looping and array challenge completed

--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
@@ -1,5 +1,25 @@
 require_relative "wagon_sort"
 
+students = []
+name = nil
+
+while name != ""
+  puts students.empty? ? "Type a student name:" : "Type another student name )or press enter to finish):"
+  name = gets.chomp
+  students << name if name != ""
+end
+
+sorted_students = wagon_sort(students)
+num_students = sorted_students.size
+
+def pluralize(number, word)
+  if number == 1
+    word
+  else
+    "#{word}s"
+  end
+end
+
 # TODO: Ask the user about students to add to the Wagon.
 #       Remember, to read an input from the command line, use:
 #       - `gets`:  https://ruby-doc.org/core-2.7.5/Kernel.html#method-i-gets
@@ -9,3 +29,11 @@ require_relative "wagon_sort"
 
 # TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
 #       file and display the sorted student list
+
+
+puts "Congratulations! Your Wagon has #{num_students} #{pluralize(num_students, 'student')}:"
+if sorted_students.size >= 2
+  puts "#{sorted_students[0..-2].join(', ')} and #{sorted_students.last}"
+else
+  puts sorted_students.first
+end
